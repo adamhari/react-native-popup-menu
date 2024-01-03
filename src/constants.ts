72049e12b8511f18c2d0365ffbdb0f7b542aade8
Adam Hari <adam.hari@kashoo.com>
2024-01-03 02:00:22 -0700

    fix: revert constants value change

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { Platform } from 'react-native';
 
 // common durations of animation
-export const OPEN_ANIM_DURATION = 150;
-export const CLOSE_ANIM_DURATION = 100;
+export const OPEN_ANIM_DURATION = 225;
+export const CLOSE_ANIM_DURATION = 195;
 
 export const USE_NATIVE_DRIVER = Platform.OS !== 'web';
